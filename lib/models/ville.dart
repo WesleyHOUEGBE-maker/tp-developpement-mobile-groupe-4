5f79b0f87b3de62f23c97a1f088c26f868e1b9eb
WesleyHOUEGBE-maker <wesleysenoumatin@gmail.com>
2026-06-26 19:16:26 +0000

    Ajout GPS, notifications, photo ville - TP Partie 3

--- lib/models/ville.dart
+++ lib/models/ville.dart
@@ -5,6 +5,8 @@ class Ville {
   final double temperature; // Température enregistrée en degrés Celsius
   final String condition;   // Conditions météo : "Ensoleille", "Nuageux", "Pluvieux"
   final int humidite;       // Taux d'humidité en pourcentage (0-100)
+  
+  final String? photoPath; // <- NOUVEAU : chemin vers la photo
 
   // Constructeur de la classe avec paramètres nommés et obligatoires
   Ville({
@@ -13,5 +15,18 @@ class Ville {
     required this.temperature,
     required this.condition,
     required this.humidite,
+    this.photoPath,
   });
+  
+  
+  // Copier la ville avec une nouvelle photo
+       VillecopierAvecPhoto(String chemin) {
+           return Ville(
+            nom: nom , pays: pays , temperature: temperature ,
+              condition: condition , humidite: humidite ,
+           photoPath: chemin ,
+          );
+
+} 
+
 }
